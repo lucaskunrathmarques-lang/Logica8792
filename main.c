58c71d1d496c93445806b6836b93efb59ee81996
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-14 20:27:12 -0300

    compara numero

--- main.c
+++ main.c
@@ -6,22 +6,17 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int idade;
+    int a, b, c, maior;
 
-    printf("digite idade: ");
-    scanf("%d", &idade);
+    printf("digite tres numeros: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-    if(idade >= 50){
-        printf("vocé é um seior");
-    }else if(idade >= 18){
-        printf("Voce é maior de idade");
-    }else if(idade >= 12){
-        printf("voce é um adolecente");
-    }else if(idade >= 9){
-        printf("voce é um pre-adolecente");
-    }else{
-        printf("voce é uma criança");
-    }
+    maior = a;
+
+    if(b > maior) maior = b;
+    if(c > maior) maior = c;
+
+    printf("maior: %d", maior);
     
 
     
