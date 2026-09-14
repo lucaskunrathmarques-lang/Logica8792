fcd73cc1fbaad01a5c4caaf163ad3cdc00a06343
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-14 19:31:36 -0300

    negativo, positivo ou igual 1L

--- main.c
+++ main.c
@@ -11,7 +11,11 @@ int main(){
     printf("digite um numero: ");
     scanf("%d", &n);
 
-    printf("%s", n > 0 ? "positivo" : (n < 0 ? "negativo" : "zero"));
+    if(n % 2 == 0){
+        printf("\npar");
+    }else{
+        printf("\nimpar");
+    }
 
     
 
