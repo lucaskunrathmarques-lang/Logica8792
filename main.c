beecc0ff6685a7c15d39d8c40b065eabbb5a75d4
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-14 21:35:50 -0300

    compara numero, maior e maneo

--- main.c
+++ main.c
@@ -6,11 +6,37 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int a, b, c, d, e, maior;
+    int a, b, c, d, e, menor, maior;
 
-    printf("digite tres numeros: ");
-    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("digite um numero para a:");
+    scanf("%d", &a);
 
+    printf("digite um numero para b:");
+    scanf("%d", &b);
+
+    printf("digite um numero para c: ");
+    scanf("%d", &c);
+
+    printf("digite um numero para d: ");
+    scanf("%d", &d);
+
+    printf("digite um numero para e: ");
+    scanf("%d", &e);
+
+    menor = a;
+
+    if(b < menor) menor = b;
+    if(c < menor) menor = c;
+    if(d < menor) menor = d;
+    if(e < menor) menor = e;
+
+    // if(b > maior) maior b
+    //esta "perguntando" se b é maior que "maior"/"a"
+    //se for maior a varialvel "maior" se torna "b"
+
+
+    printf("menor: %d\n", menor);
+    
     maior = a;
 
     if(b > maior) maior = b;
@@ -18,9 +44,7 @@ int main(){
     if(d > maior) maior = d;
     if(e > maior) maior = e;
 
-    printf("maior: %d", maior);
-    
-
+    printf("maior: %d\n", maior);  
     
 
 
