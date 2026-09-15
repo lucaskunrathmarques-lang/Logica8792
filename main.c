563f1517ebd6f8e5e31b13013ad736af25d5cece
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-14 21:47:57 -0300

    igual ou diferente

--- main.c
+++ main.c
@@ -6,46 +6,16 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int a, b, c, d, e, menor, maior;
+    int a, b;
 
-    printf("digite um numero para a:");
-    scanf("%d", &a);
+    printf("digite dois numeros");
+    scanf("%d %d", &a, &b);
 
-    printf("digite um numero para b:");
-    scanf("%d", &b);
-
-    printf("digite um numero para c: ");
-    scanf("%d", &c);
-
-    printf("digite um numero para d: ");
-    scanf("%d", &d);
-
-    printf("digite um numero para e: ");
-    scanf("%d", &e);
-
-    menor = a;
-
-    if(b < menor) menor = b;
-    if(c < menor) menor = c;
-    if(d < menor) menor = d;
-    if(e < menor) menor = e;
-
-    // if(b > maior) maior b
-    //esta "perguntando" se b é maior que "maior"/"a"
-    //se for maior a varialvel "maior" se torna "b"
-
-
-    printf("menor: %d\n", menor);
-    
-    maior = a;
-
-    if(b > maior) maior = b;
-    if(c > maior) maior = c;
-    if(d > maior) maior = d;
-    if(e > maior) maior = e;
-
-    printf("maior: %d\n", maior);  
-    
+    if(a == b){
+        printf("os numeros são iguais\n");
+    }else{
+        printf("os numeros são diferentes\n");
+    }
 
 
 
