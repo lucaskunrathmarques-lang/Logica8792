c66943bea88de54f74bed3ce6461fc2d4e09f156
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-18 20:56:58 -0300

    letras

--- main.c
+++ main.c
@@ -8,40 +8,15 @@ int main(){
 
     
 
-   int mes;
+   char letra;
+   printf("digite uma letra: ");
+   scanf("%c", &letra);
 
-   printf("digite um numero de 1 a 12\n");
-   scanf("%d", &mes);
-
-   switch (mes)
-    {   
-        case 12:
-        case 1:
-        case 2:
-            printf("verão\n");
-            break;
-
-        case 3:
-        case 4:
-        case 5:
-            printf("outono\n");
-            break;
-
-        case 6:
-        case 7:
-        case 8:
-            printf("inverno\n");
-            break;
-
-        case 9:  
-        case 10:
-        case 11:
-           printf("primavera\n");
-       
-        default:
-            printf("valor invalido");
-            break;
-    }   
+   if(letra == 'a' || letra == 'e' || letra == 'i' || letra =='o' || letra == 'u' || letra == 'A' || letra == 'I' || letra == 'E' || letra == 'O' || letra == 'U'){
+    printf("vogal\n");
+   }else{
+    printf("consoante\n");
+   }
 
 
     
