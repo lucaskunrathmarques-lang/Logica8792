4c734b124ca9ed1d3373f15921db2592f59c3ed5
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-18 20:08:09 -0300

    v.o.i.p

--- main.c
+++ main.c
@@ -8,26 +8,51 @@ int main(){
 
     
 
-    float n1, n2, n3, media;
+   int mes;
 
-    printf("digite a nota 1: ");
-    scanf("%f", &n1);
+   printf("digite um numero de 1 a 12\n");
+   scanf("%d", &mes);
 
-    printf("digite a nota 2: ");
-    scanf("%f", &n2);
+   switch (mes)
+    { 
+        case 1:
+            printf("janeiro | verão");
+            break;
+        case 2:
+            printf("fevereiro | verão");
+            break;
+        case 3:
+            printf("marco | outono");
+            break;
+        case 4:
+            printf("abril | outono");
+            break;
+        case 5:
+            printf("maio | outono");
+            break;
+        case 6:
+            printf("junho | inverno");
+            break;
+        case 7:
+            printf("julho | inverno");
+            break;
+        case 8:
+            printf("agosto | inverno");
+            break;
+        case 9:
+            printf("setembro | primavera");
+            break;
+        case 10:
+            printf("outubro | primavera");
+            break;
+        case 11:
+            printf("novembro | primavera");
+            break;
+        case 12:
+            printf("dezembro | verão");
+    }   
 
-    printf("\ndigite a nota 3: ");
-    scanf("%f", &n3);
 
-    media = (n1 + n2 + n3) / 3;
-
-    if(media >= 7){
-        printf("aprovado");
-    }else if(media >= 5){
-        printf("recuperação");
-    }else{
-        printf("reprovado");
-    }
     
 
     return 0;
