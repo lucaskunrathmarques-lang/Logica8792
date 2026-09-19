ded564f0439219b678c2943e80ef30cc6ae3623e
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-18 21:52:12 -0300

    i

--- main.c
+++ main.c
@@ -8,26 +8,8 @@ int main(){
 
     
 
-   float peso, altura, imc;
-
-   printf("digite um pedo (kg):");
-   scanf("%f", &peso);
-
-   printf("digite a altura (m)");
-   scanf("%f", &altura);
-
-   imc = peso / (altura + altura);
-
-   if(imc < 18.5){
-   printf("sue peso é: %f\n", peso);
-   printf("sua altura é: %f\n", altura );
-    printf("classificação: abaixo do peso\n");
-   }else if(imc < 25){
-    printf("classificação: peso normal\n");
-   }else if(imc < 30){
-    printf("classificação: acima do peso\n");
-   }else{
-    printf("classificação: obesidade\n");
+   for(int i = 1; i <= 100; i++){
+    printf("%d\n", i);
    }
 
     
