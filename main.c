3a0c2eb19213a305729b48cda3668cebb860b45d
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-21 20:54:16 -0300

    char, numeros in, numeros deci

--- main.c
+++ main.c
@@ -8,15 +8,34 @@ int main(){
 
     
 
-   char nomes[4][20] = {"caio", "joao", "rafael", "lucas"};
+    int numerosInteiros[5] = {1, 5, 10, 15, 20};
+    float numerosFloat[5] = {10.5, 20.4, 34.5, 21.4, 12.2};
+    char carros[3][20] = {"ferrari", "bugat", "MClarem"};
 
-   printf("%s\n", nomes[0]);
-   printf("%s\n", nomes[1]);
-   printf("%s\n", nomes[2]);
-   printf("%s\n", nomes[3]); 
-  
-   
+    printf("\nnumeros inteiros:\n\n");
+
+    printf("%d\n", numerosInteiros[0]);
+    printf("%d\n", numerosInteiros[1]);
+    printf("%d\n", numerosInteiros[2]);
+    printf("%d\n", numerosInteiros[3]);
+    printf("%d\n", numerosInteiros[4]);
+
+    printf("\nnumeros decimais:\n\n");
+
+    printf("%f\n", numerosFloat[0]);
+    printf("%f\n", numerosFloat[1]);
+    printf("%f\n", numerosFloat[2]);
+    printf("%f\n", numerosFloat[3]);
+    printf("%f\n", numerosFloat[4]);
     
+    printf("\ncarros:\n\n");
+
+    printf("%s\n", carros[0]);
+    printf("%s\n", carros[1]);
+    printf("%s\n", carros[2]);
+
+
+
 
     return 0;
  
