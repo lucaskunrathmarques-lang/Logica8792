9d6db1ead1156793ef0f7ede1310f85da1a350e6
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-21 21:34:56 -0300

    for, 'soma'

--- main.c
+++ main.c
@@ -8,11 +8,20 @@ int main(){
 
     
 
-    for(int i = 0; i <= 10; i = i + 2){
-        printf("%d\n", i);
+    int soma = 0;
+    int i;
+
+    for(i = 1; i <= 100; i++){
+        //0 + 1
+        //1 + 2
+        //3 + 3
+        //6 + 4
+        //coluna da esquerda é "soma", da direita "i", o valor de "soma" é senpre o valor que ele ganhou apos somar com "i", ou seja s = 0 + I = 1, "s" se torna 1 e isso se repete
+        //ate o i chegar ao valor maximo 
+        soma = soma + i;
     }
 
-
+    printf("soma e %d", soma);
 
 
 
