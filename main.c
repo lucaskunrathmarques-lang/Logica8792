91bf0a790221228cae90e0db60e4d37728aa6abb
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-21 21:15:01 -0300

    for '-'

--- main.c
+++ main.c
@@ -8,13 +8,9 @@ int main(){
 
     
 
-    int idades[5] = {132, 432, 523, 423, 532};
-
-    printf("\n%s %d %s", "manuel tem, ", idades[0], "anos!");
-    printf("\n%s %d %s", "rafael tem, ", idades[1], "anos!");
-    printf("\n%s %d %s", "joao tem, ", idades[2], "anos!");
-    printf("\n%s %d %s", "laura tem, ", idades[3], "anos!");
-    printf("\n%s %d %s", "caio tem, ", idades[4], "anos!");
+    for(int i = 10; i >= 0; i = i - 2){
+        printf("%d\n", i);
+    }
 
 
 
