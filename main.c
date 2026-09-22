f25134c096120cbe80204a266793a41b221fa6a9
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-21 21:04:19 -0300

    nome x idade

--- main.c
+++ main.c
@@ -8,31 +8,14 @@ int main(){
 
     
 
-    int numerosInteiros[5] = {1, 5, 10, 15, 20};
-    float numerosFloat[5] = {10.5, 20.4, 34.5, 21.4, 12.2};
-    char carros[3][20] = {"ferrari", "bugat", "MClarem"};
+    int idades[5] = {132, 432, 523, 423, 532};
 
-    printf("\nnumeros inteiros:\n\n");
+    printf("\n%s %d %s", "manuel tem, ", idades[0], "anos!");
+    printf("\n%s %d %s", "rafael tem, ", idades[1], "anos!");
+    printf("\n%s %d %s", "joao tem, ", idades[2], "anos!");
+    printf("\n%s %d %s", "laura tem, ", idades[3], "anos!");
+    printf("\n%s %d %s", "caio tem, ", idades[4], "anos!");
 
-    printf("%d\n", numerosInteiros[0]);
-    printf("%d\n", numerosInteiros[1]);
-    printf("%d\n", numerosInteiros[2]);
-    printf("%d\n", numerosInteiros[3]);
-    printf("%d\n", numerosInteiros[4]);
-
-    printf("\nnumeros decimais:\n\n");
-
-    printf("%f\n", numerosFloat[0]);
-    printf("%f\n", numerosFloat[1]);
-    printf("%f\n", numerosFloat[2]);
-    printf("%f\n", numerosFloat[3]);
-    printf("%f\n", numerosFloat[4]);
-    
-    printf("\ncarros:\n\n");
-
-    printf("%s\n", carros[0]);
-    printf("%s\n", carros[1]);
-    printf("%s\n", carros[2]);
 
 
 
