b769399c7a4674a08f2f995cfcf64b97a4f54c0d
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-22 20:58:38 -0300

    retomada de array

--- main.c
+++ main.c
@@ -2,19 +2,7 @@
 #include<windows.h>
 
 
-int somar(int a, int b){
-    return a + b;
-}
-int subtrair(int a, int b){
-    return a - b;
-}
-int dividir(int a, int b){
-    if(b <= 0){
-        printf("ele precisa ser maior que 0!");
-    }else{
-        return a / b;
-    }
-}
+
 
 
 int main(){
@@ -22,14 +10,10 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int resultadoSoma = somar(10, 15);
-    printf("o resultado da somaé: %d\n", resultadoSoma);
-    
-    int resultadoSubtração = subtrair(100, 30);
-    printf("o resultado da subtração é: %d\n", resultadoSubtração);
+    int num[5] = {1, 2, 3, 4, 5};
 
-    int resultadoDividir = dividir(100, 5);
-    printf("o resultado de dividir é: %d\n", resultadoDividir);
+    printf("%d\n", num[0]);
+    printf("%d\n", num[2]);
 
 
 
