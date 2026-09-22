05313ccef49844fae5aa455b1a568d6c556d3d15
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-22 19:25:07 -0300

    funções

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 #include<windows.h>
 
+
+void dobrar(int *x){
+        *x = *x * 2;
+}
+
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    
+    int numero = 10;
+    dobrar(&numero);
+    printf("numero depois da função %d\n", numero);
 
-    int x = 10;
-    int *p = &x;
 
-    printf("valor de x: %d\n", x);
-    printf("endereço de x: %p\n", &x);
-    printf("valor via ponteiro: %d\n", *p);
 
 
 
