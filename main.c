d5f20437e1b0437998f96c5950739f9a88fd0339
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-23 20:13:21 -0300

    tabuada 2

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include<string.h>
 #include<math.h>
 
-void tabuada(int n){
+int tabuada(int n){
+    int resultado;
     printf("tabuada do %d\n", n);
     for(int i = 1; i <= 10; i++){
-        printf("%d x %d = %d\n", n, i, n * i);
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
     }
+    return resultado;
 }
     
 
@@ -18,7 +21,8 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    tabuada(3);
+    int ultimo = tabuada(20);
+    printf("ultimo valor da tabuada: %d\n", ultimo);
 
 
 
