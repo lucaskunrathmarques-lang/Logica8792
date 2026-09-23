25794c32fa7a2b99362dc66daf3f4b3615093a06
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-22 21:38:04 -0300

    char, nomes

--- main.c
+++ main.c
@@ -10,21 +10,14 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int horas[12];
-    int minutos[60];
+    char nomes[3][30] = {
+        "nicole",
+        "eduardo",
+        "maria"
+    };
 
-    for(int i = 0; i < 12; i++){
-        horas[i] = i + 1;
-    }
-
-    for(int j = 0; j < 60; j++){
-        minutos[j] = j;
-    }
-
-    for(int i = 0; i < 12; i++){
-        for(int j = 0; j < 60; j++){
-            printf("horas: %2d:%02d\n", horas[i], minutos[j]);
-        }
+    for(int i = 0; i < 3; i++){
+        printf("%s\n", nomes[i]);
     }
 
 
