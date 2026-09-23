6a0bb8419ac7ff5e59491b0a4e082f067d18afb1
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-23 20:01:13 -0300

    tabuada

--- main.c
+++ main.c
@@ -3,16 +3,13 @@
 #include<string.h>
 #include<math.h>
 
-void fibonacci(int termos){
-    int a = 0, b = 1, c;
-    printf("seguencia de fibonacci (%d termo): \n", termos);
-    for(int i = 1; i <= termos; i++){
-        printf("%d\n", a);
-        c = a + b;
-        a = b;
-        b = c;
+void tabuada(int n){
+    printf("tabuada do %d\n", n);
+    for(int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", n, i, n * i);
     }
-}    
+}
+    
 
 
 
@@ -21,7 +18,7 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    fibonacci(10);
+    tabuada(3);
 
 
 
