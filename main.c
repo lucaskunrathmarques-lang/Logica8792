f332e8cc4bad15f069380a3b853f9888bf0be2f4
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-23 19:49:52 -0300

    fibonacci

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 #include<string.h>
 #include<math.h>
 
-    int fatorial(int n){
-        int resultado = 1;
-        for(int i = 1; i <= n; i++){
-            resultado *= i;
-        }
-        return resultado;
+void fibonacci(int termos){
+    int a = 0, b = 1, c;
+    printf("seguencia de fibonacci (%d termo): \n", termos);
+    for(int i = 1; i <= termos; i++){
+        printf("%d\n", a);
+        c = a + b;
+        a = b;
+        b = c;
     }
+}    
+
 
 
 int main(){
@@ -17,8 +21,7 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int numero = 10;
-    printf("fatorial de %d = %d\n", numero, fatorial(numero));
+    fibonacci(10);
 
 
 
