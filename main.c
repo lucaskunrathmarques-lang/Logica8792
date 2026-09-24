c348c517ad66ea07fd45e24a7bad40ca901bddcc
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-23 21:55:29 -0300

    impar par, sem função

--- main.c
+++ main.c
@@ -3,18 +3,7 @@
 #include<string.h>
 #include<math.h>
 
-void contarVogais(char palavras[]){
-    int contador = 0;
-    for(int i = 0; i <strlen(palavras); i++){
-        char c = palavras[i];
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-            contador++;
-        }
-    }
-    printf("a palavra '%s' tem %d vogais.\n", palavras, contador);
 
-}
 
     
 
@@ -25,10 +14,20 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    char palavra;
-    printf("DIGITE UMA PALAVRA: ");
-    scanf("%s", &palavra);
-    contarVogais(&palavra);
+    int n;
+
+    printf("digite um numero");
+    scanf("%d", &n);
+
+    if(n % 2 == 0){
+        printf("%d é par\n:", n);
+    }else{
+        printf("%d é impar\n:", n);
+    }
+
+
+
+
 
 
     return 0;
