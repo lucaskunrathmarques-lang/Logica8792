680d8b52c10bac0a1fae9e4afbc366b2e4528521
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-24 19:09:33 -0300

    while1

--- main.c
+++ main.c
@@ -14,16 +14,13 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int n;
+    int i = 1;
 
-    printf("digite um numero");
-    scanf("%d", &n);
-
-    if(n % 2 == 0){
-        printf("%d é par\n:", n);
-    }else{
-        printf("%d é impar\n:", n);
+    while (i <= 5){
+        printf("%d\n", i);
+        i++;
     }
+    
 
 
 
