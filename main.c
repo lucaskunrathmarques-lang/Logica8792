31891ed371dc69461eca28a99f9a33ec7e29b45a
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-24 20:08:52 -0300

    while switch

--- main.c
+++ main.c
@@ -14,16 +14,39 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int i;
+    int o = 1;
 
-    printf("digite um numero: ");
-    scanf("%d", &i);
+    while (o != 0)
+    {
+        printf("\nmenu: \n");
+        printf("1 - depositar\n");
+        printf("2 - sacar\n");
+        printf("3 - consultar\n");
+        printf("0 - sair\n");
+        printf("escolha: ");
+        scanf("%d", &o);
 
-    while(i <= 0){
-        printf("numero invalido! digite novamente:");
-        scanf("%d", &i);
+        switch (o)
+        {
+        case 1:
+            printf("voce escolheu depositar\n");
+            break;
+        case 2:
+            printf("voce estcolheu sacar\n");
+            break;
+        case 3:
+            printf("voce escolheu consultar\n");
+            break;
+        case 0:
+            printf("saindo...");
+            break;
+        default:
+            printf("opção invalida\n");
+            break;
+        }
+
     }
-    printf("numero valido: %d\n", i);
+    
 
 
 
