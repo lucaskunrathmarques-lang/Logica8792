516e47cd367dfb564c183a18d9e9c7d7e935d785
LUCAS KUNRATH MARQUES <lucaskunrathmarques@gmail.com>
2026-09-24 21:34:00 -0300

    tabuada, for

--- main.c
+++ main.c
@@ -14,17 +14,18 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int n, soma = 0;
+    int n;
 
     printf("digite um numero:");
-    scanf("%d", &n);
+    scanf("%d",  &n);
 
-    for(int i = 1; i <= n; i++){
-        soma += i;
+    for(int i = 1; i <= 10; i++){
+    printf("%d x %d = %d\n", n, i, n * i);
     }
-    printf("o resultado da soma é: %d\n", soma);
     
 
+    
+    
 
 
 
